69b47f1d2afd4406a20d24c08df29181bc56cdf4
zzgit <2489387118@qq.com>
2026-03-15 16:45:09 +0800

    静态资源引入

--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+//引入初始化样式文件
+import './styles/common.scss'
+
 //测试接口函数
 import{getCategory} from '@/apis/testAPI'
 //测试接口调用
 getCategory().then(res => {
   console.log(res)
 })
-
 
 const app = createApp(App)
 
