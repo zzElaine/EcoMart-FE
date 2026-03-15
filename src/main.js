fcac820715947cea7e62be108be9bdfa07ef898d
zzgit <2489387118@qq.com>
2026-03-15 16:09:24 +0800

    axios技术封装

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+//测试接口函数
+import{getCategory} from '@/apis/testAPI'
+//测试接口调用
+getCategory().then(res => {
+  console.log(res)
+})
+
 
 const app = createApp(App)
 
