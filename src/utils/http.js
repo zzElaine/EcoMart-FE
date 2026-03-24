0f8fb29ba23b8d54f6e6799a3fa5acd64d34efed
zzgit <2489387118@qq.com>
2026-03-24 15:09:51 +0800

    登录-token失效401报错拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,7 @@
 //axios基础封装
 import axios from 'axios'
 import {useUserStore} from '@/stores/user'
+import router from '@/router'
 const httpInstance = axios.create({
   baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout:5000
@@ -18,11 +19,19 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore = useUserStore();
   //错误提示
   ElMessage({
     type:'warning',
     message:e.response.data.message
   })
+  //401错误处理
+  // 1.清除本地用户数据
+  // 2.跳转到登录页
+  if(e.response.status === 401){
+    userStore.logout()
+    router.push('/login')
+  }
   return Promise.reject(e)
 })
 
