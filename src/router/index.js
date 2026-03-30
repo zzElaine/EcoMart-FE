eac43b83f767ab5b0334c4bc15304f24b8827cde
zzgit <2489387118@qq.com>
2026-03-30 12:17:05 +0800

    会员中心-功能理解以及路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,9 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/index.vue'
+import MemberInfo from '@/views/Member/components/MemberInfo.vue'
+import MemberOrder from '@/views/Member/components/MemberOrder.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -55,6 +58,20 @@ const router = createRouter({
         {
           path: '/paycallback',
           component: PayBack
+        },
+        {
+          path: '/member',
+          component: Member,
+          children: [
+            {
+              path: 'user',
+              component: MemberInfo
+            },
+            {
+              path: 'order',
+              component: MemberOrder
+            }
+          ]
         }
       ]
     }
